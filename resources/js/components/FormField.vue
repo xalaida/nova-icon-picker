bec09c81fd6b4311a1d45eb0b78ad149ad785b5a
nevadskiy <nevadskiy@gmail.com>
2023-08-28 21:04:44 +0300

    Add select modal

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,45 +1,72 @@
 <template>
-  <DefaultField
-    :field="field"
-    :errors="errors"
-    :show-help-text="showHelpText"
-    :full-width-content="fullWidthContent"
-  >
-    <template #field>
-      <input
-        :id="field.attribute"
-        type="text"
-        class="w-full form-control form-input form-input-bordered"
-        :class="errorClasses"
-        :placeholder="field.name"
-        v-model="value"
-      />
-    </template>
-  </DefaultField>
+    <DefaultField
+        :field="field"
+        :errors="errors"
+        :show-help-text="showHelpText"
+        :full-width-content="fullWidthContent"
+    >
+        <template #field>
+            <input
+                :id="field.attribute"
+                type="text"
+                class="w-full form-control form-input form-input-bordered"
+                :class="errorClasses"
+                :placeholder="field.name"
+                v-model="value"
+            />
+
+            <DefaultButton
+                @click="() => isSelecting = true"
+            >
+                Select icon
+            </DefaultButton>
+
+            <SelectIconModal
+                v-if="isSelecting"
+                @close="isSelecting = false"
+            />
+        </template>
+    </DefaultField>
 </template>
 
 <script>
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
+import SelectIconModal from "./SelectIconModal.vue";
 
 export default {
-  mixins: [FormField, HandlesValidationErrors],
-
-  props: ['resourceName', 'resourceId', 'field'],
-
-  methods: {
-    /*
-     * Set the initial, internal value for the field.
-     */
-    setInitialValue() {
-      this.value = this.field.value || ''
+    components: {
+        SelectIconModal
     },
 
-    /**
-     * Fill the given FormData object with the field's internal value.
-     */
-    fill(formData) {
-      formData.append(this.fieldAttribute, this.value || '')
+    mixins: [
+        FormField,
+        HandlesValidationErrors
+    ],
+
+    props: [
+        'resourceName',
+        'resourceId',
+        'field',
+    ],
+
+    data: () => ({
+        isSelecting: true
+    }),
+
+    methods: {
+        /*
+         * Set the initial, internal value for the field.
+         */
+        setInitialValue() {
+            this.value = this.field.value || ''
+        },
+
+        /**
+         * Fill the given FormData object with the field's internal value.
+         */
+        fill(formData) {
+            formData.append(this.fieldAttribute, this.value || '')
+        },
     },
-  },
 }
 </script>
---
+++ resources/js/components/SelectIconModal.vue
@@ -0,0 +1,58 @@
+<template>
+    <Modal
+        show
+        size="2xl"
+        role="alertdialog"
+        @close-via-escape="$emit('close')"
+    >
+        <div class="bg-white dark:bg-gray-800">
+            <ModalHeader class="flex items-center">
+                Select icon
+            </ModalHeader>
+
+            <ModalContent class="px-8 divide-y divide-gray-100 dark:divide-gray-800 -mx-3">
+                <ul v-if="hasOptions">
+                    <li
+                        v-for="(component, type) in [1, 2, 3]"
+                        :key="type"
+                    >
+                        <button
+                            type="button"
+                            class="w-full hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-600 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-300 py-2 px-3 no-underline font-normal text-left"
+                            @click="emits('selected', type)"
+                        >
+                            {{ component }}
+                        </button>
+                    </li>
+                </ul>
+
+                <div v-else>
+                    There are no registered components.
+                </div>
+            </ModalContent>
+
+            <ModalFooter>
+                <div class="ml-auto">
+                    <BasicButton
+                        type="button"
+                        @click="$emit('close')"
+                    >
+                        {{ __('Cancel') }}
+                    </BasicButton>
+                </div>
+            </ModalFooter>
+        </div>
+    </Modal>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+// const props = defineProps({
+//     options: { type: Object, required: true },
+// })
+
+const emits = defineEmits(['close', 'selected'])
+
+const hasOptions = computed(() => true)
+</script>
