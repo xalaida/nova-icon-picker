f8fde73dde1070d001c843f60c512c78bfd45d9b
nevadskiy <nevadskiy@gmail.com>
2023-09-02 18:50:51 +0300

    wip

--- src/Http/Controllers/IconController.php
+++ src/Http/Controllers/IconController.php
@@ -5,6 +5,7 @@ namespace Nevadskiy\Nova\Icon\Http\Controllers;
 use Nevadskiy\Nova\Icon\IconsetRegistry;
 
 // @todo add permissions.
+// @todo caching.
 class IconController
 {
     public function index(string $iconset, IconsetRegistry $registry): array
