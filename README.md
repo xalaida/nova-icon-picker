98f23d61ab6efad69843c8094bf48d777e9e943e
nevadskiy <nevadskiy@gmail.com>
2023-09-06 00:33:15 +0300

    wip

--- README.md
+++ README.md
@@ -2,23 +2,22 @@
 
 ## To Do List
 
-- [ ] support dependent field
-- [ ] support responsive layout
-- [ ] support readonly field
-- [x] support nullable icons
-- [x] index / detail views
 - [x] support removable icons
 - [x] check validation errors
 - [x] icons search bar
 - [x] configure icon size for preview
 - [x] empty icon sets
-- [ ] restyle modal
+- [x] add button to unset icon
+- [x] display icon name similar to file image field 
+- [x] support nullable icons
+- [x] index / detail views
+- [x] restyle modal
+- [x] empty icons view
+- [ ] support dependent field
+- [ ] support responsive layout
+- [ ] support readonly field
 - [ ] configure picker grid (modal size, columns, icon size)
 - [ ] ukrainian localization
 - [ ] cache API json responses
 - [ ] raw svg icons
 - [ ] Iconify API
-- [ ] display icon name similar to file image field 
-- [ ] add button to unset icon
-- [ ] handle case when no icon sets registered
-- [ ] handle case when icon not found
--- resources/js/components/SelectIconModal.vue
+++ resources/js/components/SelectIconModal.vue
@@ -50,14 +50,21 @@
                                     @click="selectIcon(icon)"
                                 >
                                     <span class="flex items-center justify-center">
-                                        <span v-html="icon.contents" class="inline-block w-10 h-10" />
+                                        <span v-html="icon.contents" class="inline-block w-10 h-10"/>
                                     </span>
                                 </button>
                             </li>
                         </ul>
 
-                        <div v-else>
-                            No icons found.
+                        <div
+                            v-else
+                            class="text-center py-3"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-12 h-12">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                            </svg>
+
+                            <p class="mt-3">No icons found.</p>
                         </div>
                     </LoadingView>
                 </div>
@@ -88,7 +95,7 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import {ref, computed, onMounted} from 'vue'
 
 const props = defineProps(['resourceName', 'attribute', 'iconsets', 'currentIconset', 'currentIcon'])
 
