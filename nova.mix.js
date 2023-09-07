52350351e5afeee1813f2aaae972fc79929e7f1e
nevadskiy <nevadskiy@gmail.com>
2023-09-07 21:18:00 +0300

    wip

--- nova.mix.js
+++ nova.mix.js
@@ -11,13 +11,6 @@ class NovaExtension {
     this.name = name
   }
 
-  webpackPlugins() {
-    return new webpack.ProvidePlugin({
-      _: 'lodash',
-      Errors: 'form-backend-validation',
-    })
-  }
-
   webpackConfig(webpackConfig) {
     webpackConfig.externals = {
       vue: 'Vue',
@@ -25,10 +18,7 @@ class NovaExtension {
 
     webpackConfig.resolve.alias = {
       ...(webpackConfig.resolve.alias || {}),
-      'laravel-nova': path.join(
-        __dirname,
-        '../../vendor/laravel/nova/resources/js/mixins/packages.js'
-      ),
+      'laravel-nova': path.join(__dirname, '../../vendor/laravel/nova/resources/js/mixins/packages.js'),
     }
 
     webpackConfig.output = {
